f9c4ad0f628b518f5b403fa2740dae7ee9c87d13
Albert Mañosa <26429103+albertms10@users.noreply.github.com>
2024-04-11 01:04:37 +0200

    feat(layout): 💄 configura la font correctament amb tailwind
    Signed-off-by: Albert Mañosa <26429103+albertms10@users.noreply.github.com>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,13 @@
+import { cn } from "@/lib/utils";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter as FontSans } from "next/font/google";
 import { PropsWithChildren } from "react";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
 
 export const metadata: Metadata = {
   title: "Associació Catalana de l’Orgue",
@@ -13,7 +17,12 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: Readonly<PropsWithChildren>) {
   return (
     <html lang="ca">
-      <body className={inter.className}>
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.variable
+        )}
+      >
         <main className="container mx-auto px-2 sm:px-4 prose">{children}</main>
       </body>
     </html>
