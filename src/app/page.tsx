5e28f1aef592be402ed8bfa4e714e9ba183ac96c
Albert Mañosa <26429103+albertms10@users.noreply.github.com>
2024-04-10 18:26:59 +0200

    feat(app): ✨ add `react-markdown` and `@tailwindcss/typography`
    Signed-off-by: Albert Mañosa <26429103+albertms10@users.noreply.github.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,23 @@
+import Markdown from "react-markdown";
+
+const markdown = `
+# Benvinguts
+
+## Segon títol
+
+El nou web de l’associació
+
+- Primer element
+- Segon
+
+1. U
+2. Dos
+`;
+
 export default function Home() {
-  return <main>Benvinguts</main>;
+  return (
+    <main className="prose">
+      <Markdown>{markdown}</Markdown>
+    </main>
+  );
 }
