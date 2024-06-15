72dc2490207776e81ede506a3bdf25a6a33a9632
Albert Mañosa <26429103+albertms10@users.noreply.github.com>
2024-06-15 21:10:31 +0200

    test(utils): 🧪 correctly use `it` to test `stripDiacritics`
    Signed-off-by: Albert Mañosa <26429103+albertms10@users.noreply.github.com>

--- src/utils/__tests__/stripDiacritics.spec.ts
+++ src/utils/__tests__/stripDiacritics.spec.ts
@@ -1,9 +1,11 @@
-import { describe, expect } from "bun:test";
+import { describe, expect, it } from "bun:test";
 import { stripDiacritics } from "../stripDiacritics";
 
 describe("stripDiacritics", () => {
-  expect(stripDiacritics("àÉ-ï_ó û")).toBe("aE-i_o u");
-  expect(stripDiacritics("què-és-lassociació-catalana-de-lorgue")).toBe(
-    "que-es-lassociacio-catalana-de-lorgue"
-  );
+  it("removes diacritic marks from a given string.", () => {
+    expect(stripDiacritics("àÉ-ï_ó û")).toBe("aE-i_o u");
+    expect(stripDiacritics("què-és-lassociació-catalana-de-lorgue")).toBe(
+      "que-es-lassociacio-catalana-de-lorgue"
+    );
+  });
 });
