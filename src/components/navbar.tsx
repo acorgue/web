c16cf26f528b8f6b28fd4a47b2a2bc438f6f8b86
Albert Mañosa <26429103+albertms10@users.noreply.github.com>
2024-06-16 17:23:35 +0200

    feat(navbar): 📱 mostra un full lliscant lateral en mòbils
    Signed-off-by: Albert Mañosa <26429103+albertms10@users.noreply.github.com>

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
 import {
   NavigationMenu,
   NavigationMenuContent,
@@ -10,84 +11,145 @@ import {
   NavigationMenuTrigger,
   navigationMenuTriggerStyle,
 } from "@/components/ui/navigation-menu";
-import { BookmarkIcon, ReaderIcon } from "@radix-ui/react-icons";
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import {
+  BookmarkIcon,
+  HamburgerMenuIcon,
+  ReaderIcon,
+} from "@radix-ui/react-icons";
 import Link from "next/link";
+import { Fragment, useState } from "react";
 
 export function Navbar() {
-  const className = navigationMenuTriggerStyle();
   return (
-    <NavigationMenu>
+    <>
+      <DesktopNavigationMenu className="hidden md:block" />
+      <MobileNavigationMenu className="block md:hidden" />
+    </>
+  );
+}
+
+const menuItems = [
+  { href: "/", label: "Inici" },
+  { href: "/qui-som", label: "Qui som" },
+  {
+    label: "Publicacions",
+    items: [
+      {
+        href: "/full",
+        label: "Full informatiu",
+        description:
+          "La publicació trimestral amb les últimes notícies exclusives per als socis.",
+        icon: <ReaderIcon className="h-6 w-6" aria-hidden="true" />,
+      },
+      {
+        href: "/publicacions",
+        label: "Notícies i articles",
+        description: "Les darreres notes d’actualitat en format blog.",
+        icon: <BookmarkIcon className="h-6 w-6" aria-hidden="true" />,
+      },
+    ],
+  },
+  { href: "/orgues", label: "Orgues" },
+  { href: "/bibliografia", label: "Bibliografia" },
+  { href: "/associacions", label: "Associacions" },
+  { href: "/formacio", label: "Formació" },
+  { href: "/cicle", label: "Cicle «Els orgues de Catalunya»" },
+];
+
+function DesktopNavigationMenu({ className }: Readonly<{ className: string }>) {
+  const triggerClassName = navigationMenuTriggerStyle();
+  return (
+    <NavigationMenu className={className}>
       <NavigationMenuList>
-        <NavigationMenuItem>
-          <Link href="/" legacyBehavior passHref>
-            <NavigationMenuLink className={className}>Inici</NavigationMenuLink>
-          </Link>
-        </NavigationMenuItem>
-        <NavigationMenuItem>
-          <Link href="/qui-som" legacyBehavior passHref>
-            <NavigationMenuLink className={className}>
-              Qui som
-            </NavigationMenuLink>
-          </Link>
-        </NavigationMenuItem>
-        <NavigationMenuItem>
-          <NavigationMenuTrigger>Publicacions</NavigationMenuTrigger>
-          <NavigationMenuContent>
-            <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px]">
-              <NavigationMenuListItem
-                href="/full"
-                title="Full informatiu"
-                icon={<ReaderIcon className="h-6 w-6" aria-hidden="true" />}
-              >
-                La publicació trimestral amb les últimes notícies exclusives per
-                als socis.
-              </NavigationMenuListItem>
-              <NavigationMenuListItem
-                href="/publicacions"
-                title="Notícies i articles"
-                icon={<BookmarkIcon className="h-6 w-6" aria-hidden="true" />}
-              >
-                Les darreres notes d’actualitat en format blog.
-              </NavigationMenuListItem>
-            </ul>
-          </NavigationMenuContent>
-        </NavigationMenuItem>
-        <NavigationMenuItem>
-          <Link href="/orgues" legacyBehavior passHref>
-            <NavigationMenuLink className={className}>
-              Orgues
-            </NavigationMenuLink>
-          </Link>
-        </NavigationMenuItem>
-        <NavigationMenuItem>
-          <Link href="/bibliografia" legacyBehavior passHref>
-            <NavigationMenuLink className={className}>
-              Bibliografia
-            </NavigationMenuLink>
-          </Link>
-        </NavigationMenuItem>
-        <NavigationMenuItem>
-          <Link href="/associacions" legacyBehavior passHref>
-            <NavigationMenuLink className={className}>
-              Associacions
-            </NavigationMenuLink>
-          </Link>
-        </NavigationMenuItem>
-        <NavigationMenuItem>
-          <Link href="/formacio" legacyBehavior passHref>
-            <NavigationMenuLink className={className}>
-              Formació
-            </NavigationMenuLink>
-          </Link>
-        </NavigationMenuItem>
-        <NavigationMenuItem>
-          <Link href="/cicle" legacyBehavior passHref>
-            <NavigationMenuLink className={className}>
-              Cicle «Els orgues de Catalunya»
-            </NavigationMenuLink>
-          </Link>
-        </NavigationMenuItem>
+        {menuItems.map((menuItem) => (
+          <Fragment key={menuItem.label}>
+            <NavigationMenuItem>
+              {menuItem.items ? (
+                <>
+                  <NavigationMenuTrigger>
+                    {menuItem.label}
+                  </NavigationMenuTrigger>
+                  <NavigationMenuContent>
+                    <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px]">
+                      {menuItem.items.map((item) => (
+                        <NavigationMenuListItem
+                          key={item.label}
+                          href={item.href}
+                          title={item.label}
+                          icon={item.icon}
+                        >
+                          {item.description}
+                        </NavigationMenuListItem>
+                      ))}
+                    </ul>
+                  </NavigationMenuContent>
+                </>
+              ) : (
+                <Link href={menuItem.href!} legacyBehavior passHref>
+                  <NavigationMenuLink className={triggerClassName}>
+                    {menuItem.label}
+                  </NavigationMenuLink>
+                </Link>
+              )}
+            </NavigationMenuItem>
+          </Fragment>
+        ))}
       </NavigationMenuList>
     </NavigationMenu>
+  );
+}
+
+function MobileNavigationMenu({ className }: Readonly<{ className: string }>) {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <div className={className}>
+      <Sheet open={open} onOpenChange={setOpen}>
+        <SheetTrigger asChild>
+          <Button variant="outline" size="icon">
+            <HamburgerMenuIcon />
+          </Button>
+        </SheetTrigger>
+        <SheetContent side="left">
+          <SheetHeader>
+            <SheetTitle className="text-left">ACO</SheetTitle>
+          </SheetHeader>
+
+          {/* TODO: simplify flatMap nested operation. */}
+          {menuItems.flatMap((menuItem) =>
+            menuItem.items ? (
+              menuItem.items.map((item) => (
+                <Button
+                  key={item.label}
+                  className="block"
+                  variant="link"
+                  asChild
+                  onClick={() => setOpen(false)}
+                >
+                  <Link href={item.href}>{item.label}</Link>
+                </Button>
+              ))
+            ) : (
+              <Button
+                key={menuItem.label}
+                className="block"
+                variant="link"
+                asChild
+                onClick={() => setOpen(false)}
+              >
+                <Link href={menuItem.href!}>{menuItem.label}</Link>
+              </Button>
+            ),
+          )}
+        </SheetContent>
+      </Sheet>
+    </div>
   );
 }
