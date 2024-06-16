58955d31792e0d4d97e64165e672ee7270c4786c
Albert Mañosa <26429103+albertms10@users.noreply.github.com>
2024-06-16 03:17:36 -0500

    feat(layout): 💄 afegeix la barra de navegació principal (#15)
    * feat(layout): 💄 afegeix la barra de navegació principal
    
    Signed-off-by: Albert Mañosa <26429103+albertms10@users.noreply.github.com>
    
    * chore(package): 🚚 afegeix `@radix-ui/react-navigation-menu`
    
    Signed-off-by: Albert Mañosa <26429103+albertms10@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Albert Mañosa <26429103+albertms10@users.noreply.github.com>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Navbar } from "@/components/navbar";
 import { cn } from "@/lib/utils";
 import type { Metadata } from "next";
 import { Inter as FontSans } from "next/font/google";
@@ -24,7 +25,12 @@ export default function RootLayout({ children }: Readonly<PropsWithChildren>) {
           fontSans.variable,
         )}
       >
-        <main className="container mx-auto px-2 sm:px-4 prose">{children}</main>
+        <header className="fixed w-full p-4 backdrop-blur-md bg-white/50 border-b">
+          <Navbar />
+        </header>
+        <main className="container pt-32 mx-auto px-2 sm:px-4 prose">
+          {children}
+        </main>
       </body>
     </html>
   );
