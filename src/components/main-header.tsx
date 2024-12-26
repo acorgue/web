06e78fa8e7bfdc8b81c805d59e3e41c96875a555
Albert Mañosa <26429103+albertms10@users.noreply.github.com>
2024-12-26 11:19:52 +0100

    feat(profile-menu): 💄 afegeix el menú de l’espai per a membres
    Signed-off-by: Albert Mañosa <26429103+albertms10@users.noreply.github.com>

--- src/components/main-header.tsx
+++ src/components/main-header.tsx
@@ -3,7 +3,7 @@ import { getTranslations } from "next-intl/server";
 import Image from "next/image";
 import Link from "next/link";
 import { ReactNode } from "react";
-import { ModeToggle } from "./mode-toggle";
+import { ProfileMenu } from "./profile-menu";
 import { Navbar } from "./navbar";
 import { Button } from "./ui/button";
 import {
@@ -39,7 +39,7 @@ export async function MainHeader({ nav }: Readonly<MainHeaderProps>) {
           <nav className="flex items-center gap-4 text-sm xl:gap-6">{nav}</nav>
         </div>
         <Drawer>
-          <DrawerTrigger className="inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md font-medium transition-colors focus-visible:outline-none focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 hover:text-accent-foreground py-2 -ml-2 mr-2 h-8 w-8 px-0 text-base hover:bg-transparent focus-visible:bg-transparent focus-visible:ring-0 focus-visible:ring-offset-0 md:hidden">
+          <DrawerTrigger className="inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md font-medium transition-colors focus-visible:outline-none focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 hover:text-accent-foreground py-2 -ms-2 mr-2 h-8 w-8 px-0 text-base hover:bg-transparent focus-visible:bg-transparent focus-visible:ring-0 focus-visible:ring-offset-0 md:hidden">
             <HamburgerMenuIcon />
             <span className="sr-only">{t("navbar.toggleMenu")}</span>
           </DrawerTrigger>
@@ -63,8 +63,8 @@ export async function MainHeader({ nav }: Readonly<MainHeaderProps>) {
               </kbd>
             </Button>
           </div>
-          <nav className="flex items-center gap-0.5">
-            <ModeToggle />
+          <nav className="flex items-center gap-0.5 -me-2">
+            <ProfileMenu />
           </nav>
         </div>
       </div>
