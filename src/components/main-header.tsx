07c7419d494379c204449d60d0d921f0abba7fd5
Albert Mañosa <26429103+albertms10@users.noreply.github.com>
2024-12-26 09:38:39 +0100

    feat(main-header): 💄 ajusta la mida de la barra de cerca
    Signed-off-by: Albert Mañosa <26429103+albertms10@users.noreply.github.com>

--- src/components/main-header.tsx
+++ src/components/main-header.tsx
@@ -54,8 +54,8 @@ export async function MainHeader({ nav }: Readonly<MainHeaderProps>) {
             </div>
           </DrawerContent>
         </Drawer>
-        <div className="flex flex-1 items-center justify-between gap-2 md:justify-end">
-          <div className="max-w-[300px] flex-1 md:w-auto md:flex-none">
+        <div className="flex flex-1 items-center gap-2 justify-end">
+          <div className="w-full md:max-w-[300px] flex-1 md:w-auto md:flex-none">
             <Button className="inline-flex items-center gap-2 whitespace-nowrap transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 border border-input hover:bg-accent hover:text-accent-foreground px-4 py-2 relative h-8 w-full justify-start rounded-[0.5rem] bg-muted/50 text-sm font-normal text-muted-foreground shadow-none sm:pr-12 md:w-40 lg:w-56 xl:w-64">
               <span>{t("navbar.search")}</span>
               <kbd className="pointer-events-none absolute right-[0.3rem] top-[0.3rem] hidden h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium opacity-100 sm:flex">
