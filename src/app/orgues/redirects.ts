1399db1a1241d55af1869a60bf92521d53206688
Albert Mañosa <26429103+albertms10@users.noreply.github.com>
2024-12-31 00:32:09 +0100

    feat(middleware): ↩️ afegeix les redireccions d’URL d’orgue curtes (#57)
    * feat(middleware): ↩️ afegeix les redireccions d’URL d’orgue curtes
    
    Signed-off-by: Albert Mañosa <26429103+albertms10@users.noreply.github.com>
    
    * feat(orgues): ↩️ computa les redireccions d’orgues
    
    Signed-off-by: Albert Mañosa <26429103+albertms10@users.noreply.github.com>
    
    * refactor(redirects): ♻️ reanomena `municipi`
    
    Signed-off-by: Albert Mañosa <26429103+albertms10@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Albert Mañosa <26429103+albertms10@users.noreply.github.com>

---
+++ src/app/orgues/redirects.ts
@@ -0,0 +1,67 @@
+import rawOrgues from "@/database/orgues.json";
+import { OrguesEdifici, OrguesProvincia } from "./orgueNavigation";
+
+export interface RedirectEntry {
+  permanent: boolean;
+  destination: string;
+}
+
+/** Short slug to destination ({@link RedirectEntry}). */
+export const redirects = Object.fromEntries(computeRedirects());
+
+function redirectEntry(
+  source: string,
+  destinationPaths: string[],
+): [string, RedirectEntry] {
+  return [
+    `/orgues/${source}`,
+    { destination: `/orgues/${destinationPaths.join("/")}`, permanent: true },
+  ];
+}
+
+function* computeRedirects() {
+  for (const provincia of rawOrgues.orgues as OrguesProvincia[]) {
+    for (const comarca of provincia.comarques) {
+      if (!comarca.poblacions) continue;
+      for (const municipi of comarca.poblacions) {
+        if (municipi.edificis.length === 0) continue;
+        if (municipi.edificis.length === 1) {
+          const [edifici] = municipi.edificis as OrguesEdifici[];
+          if (!edifici.orgues) {
+            yield redirectEntry(
+              municipi.link,
+              [provincia, comarca, municipi, edifici].map((e) => e.link),
+            );
+          } else {
+            for (const orgue of edifici.orgues) {
+              yield redirectEntry(
+                `${municipi.link}-${orgue.link}`,
+                [provincia, comarca, municipi, edifici, orgue].map(
+                  (e) => e.link,
+                ),
+              );
+            }
+          }
+        } else {
+          for (const edifici of municipi.edificis as OrguesEdifici[]) {
+            if (!edifici.orgues) {
+              yield redirectEntry(
+                `${edifici.link}-${municipi.link}`,
+                [provincia, comarca, municipi, edifici].map((e) => e.link),
+              );
+            } else {
+              for (const orgue of edifici.orgues) {
+                yield redirectEntry(
+                  `${edifici.link}-${municipi.link}-${orgue.link}`,
+                  [provincia, comarca, municipi, edifici, orgue].map(
+                    (e) => e.link,
+                  ),
+                );
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
