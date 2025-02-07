9d245a0824269fa8e9f11810d1740c041b2e29fb
Albert Mañosa <26429103+albertms10@users.noreply.github.com>
2025-02-07 01:56:19 +0100

    refactor(footer): ♻️ extreu els components reutilitzables
    Signed-off-by: Albert Mañosa <26429103+albertms10@users.noreply.github.com>

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -7,6 +7,7 @@ import {
 } from "lucide-react";
 import { getTranslations } from "next-intl/server";
 import Link from "next/link";
+import { PropsWithChildren } from "react";
 import { Button } from "./ui/button";
 
 export default async function Footer({ className }: { className?: string }) {
@@ -14,13 +15,14 @@ export default async function Footer({ className }: { className?: string }) {
 
   return (
     <footer
-      className={cn("bg-gray-700 text-gray-300 py-12 text-sm", className)}
+      className={cn(
+        "bg-gray-800 dark:bg-gray-200 text-gray-300 dark:text-gray-700 py-12 text-sm",
+        className,
+      )}
     >
       <div className="max-w-4xl mx-auto px-6 grid grid-cols-1 md:grid-cols-3 gap-8">
         <div>
-          <h2 className="text-lg font-semibold text-white">
-            {t("footer.contactWithUs")}
-          </h2>
+          <FooterHeading>{t("footer.contactWithUs")}</FooterHeading>
           <p className="mt-2">Rocafort, 242 bis, 08029 Barcelona</p>
 
           <Button asChild variant="secondary" size="sm">
@@ -30,115 +32,90 @@ export default async function Footer({ className }: { className?: string }) {
           </Button>
 
           <ul className="flex gap-2 mt-4">
-            <li>
-              <Link href="mailto:aco@orgue.cat" className="hover:text-white">
-                <MailIcon />
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="https://www.facebook.com/acorgue/"
-                className="hover:text-white"
-              >
-                <FacebookIcon />
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="https://twitter.com/acorgue"
-                className="hover:text-white"
-              >
-                <TwitterIcon />
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="https://www.instagram.com/acorgue/"
-                className="hover:text-white"
-              >
-                <InstagramIcon />
-              </Link>
-            </li>
+            <FooterListItem href="mailto:aco@orgue.cat">
+              <MailIcon />
+            </FooterListItem>
+            <FooterListItem href="https://www.facebook.com/acorgue/">
+              <FacebookIcon />
+            </FooterListItem>
+            <FooterListItem href="https://twitter.com/acorgue">
+              <TwitterIcon />
+            </FooterListItem>
+            <FooterListItem href="https://www.instagram.com/acorgue/">
+              <InstagramIcon />
+            </FooterListItem>
           </ul>
         </div>
 
         <nav>
-          <h2 className="text-lg font-semibold text-white">
-            {t("footer.links")}
-          </h2>
+          <FooterHeading>{t("footer.links")}</FooterHeading>
           <ul className="mt-2 space-y-1">
-            <li>
-              <Link href="/associacions" className="hover:text-white">
-                {t("metadata.associations")}
-              </Link>
-            </li>
-            <li>
-              <Link href="/bibliografia" className="hover:text-white">
-                {t("metadata.references")}
-              </Link>
-            </li>
-            <li>
-              <Link href="/cicle" className="hover:text-white">
-                {t("metadata.concertSeries")}
-              </Link>
-            </li>
-            <li>
-              <Link href="/formacio" className="hover:text-white">
-                {t("metadata.education")}
-              </Link>
-            </li>
-            <li>
-              <Link href="/full" className="hover:text-white">
-                {t("metadata.factSheet")}
-              </Link>
-            </li>
-            <li>
-              <Link href="/orgues" className="hover:text-white">
-                {t("metadata.pipeOrgans")}
-              </Link>
-            </li>
-            <li>
-              <Link href="/noticies" className="hover:text-white">
-                {t("metadata.news")}
-              </Link>
-            </li>
-            <li>
-              <Link href="/qui-som" className="hover:text-white">
-                {t("metadata.aboutUs")}
-              </Link>
-            </li>
+            <FooterListItem href="/associacions">
+              {t("metadata.associations")}
+            </FooterListItem>
+            <FooterListItem href="/bibliografia">
+              {t("metadata.references")}
+            </FooterListItem>
+            <FooterListItem href="/cicle">
+              {t("metadata.concertSeries")}
+            </FooterListItem>
+            <FooterListItem href="/formacio">
+              {t("metadata.education")}
+            </FooterListItem>
+            <FooterListItem href="/full">
+              {t("metadata.factSheet")}
+            </FooterListItem>
+            <FooterListItem href="/orgues">
+              {t("metadata.pipeOrgans")}
+            </FooterListItem>
+            <FooterListItem href="/noticies">
+              {t("metadata.news")}
+            </FooterListItem>
+            <FooterListItem href="/qui-som">
+              {t("metadata.aboutUs")}
+            </FooterListItem>
           </ul>
         </nav>
 
         <div>
-          <h2 className="text-lg font-semibold text-white">
-            {t("footer.legal")}
-          </h2>
+          <FooterHeading>{t("footer.legal")}</FooterHeading>
           <ul className="mt-2 space-y-1">
-            <li>
-              <Link href="/avis-legal" className="hover:text-white">
-                {t("metadata.legalNotice")}
-              </Link>
-            </li>
-            <li>
-              <Link href="/politica-de-privacitat" className="hover:text-white">
-                {t("metadata.privacyPolicy")}
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="/politica-de-privacitat-socis"
-                className="hover:text-white"
-              >
-                {t("metadata.privacyPolicyMembers")}
-              </Link>
-            </li>
+            <FooterListItem href="/avis-legal">
+              {t("metadata.legalNotice")}
+            </FooterListItem>
+            <FooterListItem href="/politica-de-privacitat">
+              {t("metadata.privacyPolicy")}
+            </FooterListItem>
+            <FooterListItem href="/politica-de-privacitat-socis">
+              {t("metadata.privacyPolicyMembers")}
+            </FooterListItem>
           </ul>
-          <p className="mt-6 text-gray-400">
+          <p className="mt-6 text-gray-400 dark:text-gray-600">
             © 1992–2024 <br /> {t("organization.name")}
           </p>
         </div>
       </div>
     </footer>
+  );
+}
+
+function FooterHeading({ children }: Readonly<PropsWithChildren>) {
+  return (
+    <h2 className="text-lg font-semibold text-white dark:text-black">
+      {children}
+    </h2>
+  );
+}
+
+function FooterListItem({
+  href,
+  children,
+}: Readonly<PropsWithChildren<{ href: string }>>) {
+  return (
+    <li>
+      <Link href={href} className="hover:text-white hover:dark:text-black">
+        {children}
+      </Link>
+    </li>
   );
 }
